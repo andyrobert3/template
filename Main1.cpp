5c2cdd105ee813fd7b9f3becefb8200d759f4c3c
Matthew Alexander <alexandermatthew12@gmail.com>
2021-04-05 22:48:25 +0800

    Update time complexity explanation

--- Main1.cpp
+++ Main1.cpp
@@ -2,14 +2,15 @@
 	Name: Matthew Alexander
 	Describe your algorithm and explain your time complexity here:
 
-  Algorithm time complexity: O(N log N)
-  - Combines input array a & b to create array of (a_i, b_i) pairs
-	- Sorts array of pairs by a_i (increasing) -> stores in ab_sorted_a
-  - For each prefix of the items, calculate the min & max number (bound) of elements i < j to maintain property of S 
-  - For each bound at index i:
-    - Push b_i to the maximum priority queue
+  Algorithm time complexity: O(N log N) 
+  - Combines input array a & b to create array of (a_i, b_i) pairs [O(N)]
+	- Sorts array of pairs by a_i (increasing) -> stores in ab_sorted_a [O(N log N)]
+  - For each prefix of the items, calculate the min & max number (bound) of elements i < j to maintain property of S [O(N)]
+  
+	- For each bound at index i: [O(N log N)]
+    - Push b_i to the maximum priority queue [O(log N)]
     - Check if size of set S is smaller than lower bound size for that prefix
-      - If yes: Extract the biggest b_i in the priority queue to the set S
+      - If yes: Extract the biggest b_i in the priority queue to the set S [O(log N)]
   - Sum all elements in set S to get answer
 */
 
@@ -62,13 +63,15 @@ int main() {
 		bounds[i] = make_pair(lower_bound, upper_bound);
 	}
 
-
+	// O(N * log N)
   for (int i = 0; i < n; i++) {
     // Priority queue used to get greatest value of b when deciding which b_i to push to set S
+		// O(log N)
     pq.push(ab_sorted_a[i].second);
     int lower_bound = bounds[i].first;
 
     // Decide to push to S only to maintain property of S 
+		// O(log N)
     if (S.size() < lower_bound) {
       S.push_back(pq.top());
       answer += pq.top();
